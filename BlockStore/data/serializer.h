fca395b46565988bfaa3c38d81dec7b16aad4487
Chenqi <1002652154@qq.com>
2026-03-25 10:42:08 +0100

    wrap serialization context

--- BlockStore/data/serializer.h
+++ BlockStore/data/serializer.h
@@ -26,25 +26,30 @@ private:
 	const T& object;
 public:
 	static_assert(layout_dynamic<T> || layout_traits<T>::size() <= block_size_limit, "block size exceeds limit");
+private:
+	struct Context {
+		size_t size;
+		size_t ref_size;
+	};
 public:
 	std::pair<size_t, size_t> Get() const {
-		size_t size = 0; size_t ref_size = 0;
-		access(size, ref_size, object);
-		if (size > block_size_limit) {
+		Context context = { 0, 0 };
+		access(context, object);
+		if (context.size > block_size_limit) {
 			throw std::invalid_argument("block size exceeds limit");
 		}
-		return std::make_pair(size, ref_size);
+		return std::make_pair(context.size, context.ref_size);
 	}
 private:
-	static void access(size_t& size, size_t& ref_size, const layout_trivial auto& object) {
-		size += layout_traits<std::remove_cvref_t<decltype(object)>>::size();
+	static void access(Context& context, const layout_trivial auto& object) {
+		context.size += layout_traits<std::remove_cvref_t<decltype(object)>>::size();
 	}
-	static void access(size_t& size, size_t& ref_size, const block_ref& object) {
-		access(size, ref_size, static_cast<ref_t>(object));
-		ref_size++;
+	static void access(Context& context, const block_ref& object) {
+		access(context, static_cast<ref_t>(object));
+		context.ref_size++;
 	}
-	static void access(size_t& size, size_t& ref_size, const auto& object) {
-		layout_traits<std::remove_cvref_t<decltype(object)>>::read([&](const auto& item) { access(size, ref_size, item); }, object);
+	static void access(Context& context, const auto& object) {
+		layout_traits<std::remove_cvref_t<decltype(object)>>::read([&](const auto& item) { access(context, item); }, object);
 	}
 };
 
@@ -56,28 +61,34 @@ public:
 private:
 	BlockManager& manager;
 	const T& object;
+private:
+	struct Context {
+		BlockManager& manager;
+		std::vector<std::byte> data;
+		std::vector<ref_t> ref;
+	};
 public:
 	std::pair<std::vector<std::byte>, std::vector<ref_t>> Get() const {
+		Context context = { manager };
 		auto [size, ref_size] = BlockSize(object).Get();
-		std::vector<std::byte> data; data.reserve(size);
-		std::vector<ref_t> ref; ref.reserve(ref_size);
-		access(manager, data, ref, object);
-		return std::make_pair(data, ref);
+		context.data.reserve(size); context.ref.reserve(ref_size);
+		access(context, object);
+		return std::make_pair(std::move(context.data), std::move(context.ref));
 	}
 private:
-	static void access(BlockManager& manager, std::vector<std::byte>& data, std::vector<ref_t>& ref, const layout_trivial auto& object) {
+	static void access(Context& context, const layout_trivial auto& object) {
 		auto bytes = std::bit_cast<std::array<std::byte, sizeof(object)>>(object);
-		data.insert(data.end(), bytes.begin(), bytes.end());
+		context.data.insert(context.data.end(), bytes.begin(), bytes.end());
 	}
-	static void access(BlockManager& manager, std::vector<std::byte>& data, std::vector<ref_t>& ref, const block_ref& object) {
-		if (&manager != &object.get_manager()) {
+	static void access(Context& context, const block_ref& object) {
+		if (&context.manager != &object.get_manager()) {
 			throw std::invalid_argument("block manager mismatch");
 		}
-		access(manager, data, ref, static_cast<ref_t>(object));
-		ref.push_back(object);
+		access(context, static_cast<ref_t>(object));
+		context.ref.push_back(object);
 	}
-	static void access(BlockManager& manager, std::vector<std::byte>& data, std::vector<ref_t>& ref, const auto& object) {
-		layout_traits<std::remove_cvref_t<decltype(object)>>::read([&](const auto& item) { access(manager, data, ref, item); }, object);
+	static void access(Context& context, const auto& object) {
+		layout_traits<std::remove_cvref_t<decltype(object)>>::read([&](const auto& item) { access(context, item); }, object);
 	}
 };
 
@@ -89,30 +100,36 @@ public:
 private:
 	BlockManager& manager;
 	const std::vector<std::byte>& data;
+private:
+	struct Context {
+		BlockManager& manager;
+		const std::vector<std::byte>& data;
+		std::vector<std::byte>::const_iterator index;
+	};
 public:
 	T Get() const {
 		T object;
-		std::vector<std::byte>::const_iterator index = data.begin();
-		access(manager, data, index, object);
+		Context context = { manager, data, data.begin() };
+		access(context, object);
 		return object;
 	}
 private:
-	static void access(BlockManager& manager, const std::vector<std::byte>& data, std::vector<std::byte>::const_iterator& index, layout_trivial auto& object) {
-		if (data.end() < index + sizeof(object)) {
+	static void access(Context& context, layout_trivial auto& object) {
+		if (context.data.end() < context.index + sizeof(object)) {
 			throw std::runtime_error("deserialization error");
 		}
 		std::array<std::byte, sizeof(object)> bytes;
-		std::copy(index, index + sizeof(object), bytes.begin());
+		std::copy(context.index, context.index + sizeof(object), bytes.begin());
 		object = std::bit_cast<std::remove_cvref_t<decltype(object)>>(bytes);
-		index += sizeof(object);
+		context.index += sizeof(object);
 	}
-	static void access(BlockManager& manager, const std::vector<std::byte>& data, std::vector<std::byte>::const_iterator& index, block_ref& object) {
+	static void access(Context& context, block_ref& object) {
 		ref_t ref;
-		access(manager, data, index, ref);
-		object = block_ref_deserialize::construct(manager, ref);
+		access(context, ref);
+		object = block_ref_deserialize::construct(context.manager, ref);
 	}
-	static void access(BlockManager& manager, const std::vector<std::byte>& data, std::vector<std::byte>::const_iterator& index, auto& object) {
-		layout_traits<std::remove_cvref_t<decltype(object)>>::write([&](auto& item) { access(manager, data, index, item); }, object);
+	static void access(Context& context, auto& object) {
+		layout_traits<std::remove_cvref_t<decltype(object)>>::write([&](auto& item) { access(context, item); }, object);
 	}
 };
 
